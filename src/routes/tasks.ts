d996940b6c3186f032c2556a9fd32cc73ff4f1f0
Aidan Torrence <aidan.torrence@gmail.com>
2022-03-08 22:20:45 -0500

    endpoint for number of tasks completed today

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,12 +1,31 @@
 import Router from "express-promise-router";
 import { PrismaClient } from "@prisma/client";
+import { DateTime } from "luxon";
+
+const startOfDay = DateTime.now().startOf("day").toUTC().toISO();
 
 const prisma = new PrismaClient();
 
 const tasks = Router();
-tasks.get("/tasks", async (req, res) => {
+tasks.get("/tasks-completed-today", async (req, res) => {
 	try {
-		const tasks = await prisma.task.findMany();
+		const tasks = await prisma.task.findMany({
+			where: {
+				createdAt: {
+					gt: startOfDay,
+				},
+			},
+		});
+		// const records = await prisma.task.aggregate({
+		// 	_count: {
+		// 		content: true,
+		// 	},
+		// 	where: {
+		// 		createdAt: {
+		// 			gt: startOfDay,
+		// 		},
+		// 	},
+		// });
 		res.json(tasks);
 	} catch (e) {
 		res.json(e);
@@ -23,6 +42,5 @@ tasks.post("/tasks", async (req, res) => {
 		res.status(400).send("task failed");
 	}
 });
-
 
 export default tasks;
