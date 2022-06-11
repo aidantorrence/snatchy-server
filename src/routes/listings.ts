38bc5b0ad1797c950444f7cca97a8ea533678e59
Aidan Torrence <aidan.torrence@gmail.com>
2022-06-11 12:33:10 -0400

    one listing

--- src/routes/listings.ts
+++ src/routes/listings.ts
@@ -6,46 +6,60 @@ const prisma = new PrismaClient();
 
 const listings = Router();
 listings.get("/listings", async (req, res) => {
-	try {
-		const listings = await prisma.listing.findMany({
-			include: {
-				owner: true, // Return all fields
-			},
-		});
-		res.json(listings);
-	} catch (e) {
-		res.json(e);
-	}
+  try {
+    const listings = await prisma.listing.findMany({
+      include: {
+        owner: true, // Return all fields
+      },
+    });
+    res.json(listings);
+  } catch (e) {
+    res.json(e);
+  }
+});
+
+listings.get("/listing/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const listing = await prisma.listing.findUnique({
+      where: {
+        id: parseInt(id, 10),
+      },
+    });
+    res.json(listing);
+  } catch (e) {
+    res.json(e);
+  }
 });
 
 listings.get("/listings-completed-today", async (req, res) => {
-	const startOfDay = DateTime.now().startOf("day").toUTC().toISO();
-	try {
-		const listing = await prisma.$queryRaw`
+  const startOfDay = DateTime.now().startOf("day").toUTC().toISO();
+  try {
+    const listing = await prisma.$queryRaw`
 			SELECT COUNT(id) FROM "listing"
             WHERE DATE("lastReviewedDate" at time zone 'utc' at time zone 'est') = DATE(NOW() at time zone 'utc' at time zone 'est')
 			AND DATE("reviewDate" at time zone 'utc' at time zone 'est') <> DATE(NOW() at time zone 'utc' at time zone 'est')
         `;
-		res.json(listing);
-	} catch (e) {
-		res.json(e);
-	}
+    res.json(listing);
+  } catch (e) {
+    res.json(e);
+  }
 });
 
 listings.post("/listing", async (req, res) => {
-	try {
-		const listings = await prisma.listing.create({
-			data: req.body,
-		});
-		res.status(200).send("listing created");
-	} catch (e) {
-		res.status(400).send("listing failed");
-	}
+  try {
+    const listings = await prisma.listing.create({
+      data: req.body,
+    });
+    res.status(200).send("listing created");
+  } catch (e) {
+    res.status(400).send("listing failed");
+  }
 });
 
 listings.get("/listing-to-review", async (req, res) => {
-	try {
-		const listing = await prisma.$queryRaw`
+  try {
+    const listing = await prisma.$queryRaw`
 			SELECT * FROM "listing"
             WHERE "reviewDate" = (
 				SELECT min("reviewDate") 
@@ -55,24 +69,24 @@ listings.get("/listing-to-review", async (req, res) => {
 			)
 			LIMIT 1
         `;
-		// WHERE ("updatedAt" < NOW() - INTERVAL '6 hours' OR EXTRACT (epoch from ("updatedAt" - "createdAt")) < 60)
-		res.json(listing);
-	} catch (e) {
-		res.json(e);
-	}
+    // WHERE ("updatedAt" < NOW() - INTERVAL '6 hours' OR EXTRACT (epoch from ("updatedAt" - "createdAt")) < 60)
+    res.json(listing);
+  } catch (e) {
+    res.json(e);
+  }
 });
 
 listings.patch("/listing", async (req, res) => {
-	const { id } = req.body;
-	try {
-		const listing = await prisma.listing.update({
-			where: { id },
-			data: req.body,
-		});
-		res.json(listing);
-	} catch (e) {
-		res.json(e);
-	}
+  const { id } = req.body;
+  try {
+    const listing = await prisma.listing.update({
+      where: { id },
+      data: req.body,
+    });
+    res.json(listing);
+  } catch (e) {
+    res.json(e);
+  }
 });
 // listings.patch("/filter-listings", async (req, res) => {
 // 	const ids = req.body;
@@ -90,15 +104,15 @@ listings.patch("/listing", async (req, res) => {
 // });
 
 listings.delete("/listing", async (req, res) => {
-	const { id } = req.body;
-	try {
-		const listing = await prisma.listing.delete({
-			where: { id },
-		});
-		res.json(listing);
-	} catch (e) {
-		res.json(e);
-	}
+  const { id } = req.body;
+  try {
+    const listing = await prisma.listing.delete({
+      where: { id },
+    });
+    res.json(listing);
+  } catch (e) {
+    res.json(e);
+  }
 });
 
 export default listings;
