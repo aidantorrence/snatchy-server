aa26d406b47c0716e21aca3add6545d388637923
Aidan Torrence <aidan.torrence@gmail.com>
2022-07-02 23:18:56 -0400

    db update

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,20 +5,36 @@ const prisma = new PrismaClient();
 
 const users = Router();
 
-users.get('/user/:id', async (req , res) => {
-	try {
-		const user = await prisma.user.findUnique({
+users.get("/user/:id", async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
       where: {
         id: parseInt(req.params.id, 10),
       },
       include: {
         listings: true,
       },
-    })
+    });
     res.json(user);
-	} catch (e) {
-		res.json(e);
-	}
+  } catch (e) {
+    res.json(e);
+  }
 });
 
-export default users
+users.patch("/user", async (req, res) => {
+  const { id } = req.body;
+  try {
+    const listing = await prisma.user.update({
+      where: {
+        id: parseInt(id, 10),
+      },
+      data: req.body,
+    });
+    res.status(200).send(listing);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send("listing failed");
+  }
+});
+
+export default users;
