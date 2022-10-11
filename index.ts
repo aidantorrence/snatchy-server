c494b2e16c403cda99b7fa81fe025853bf3e47b3
Aidan Torrence <aidan.torrence@gmail.com>
2022-10-11 03:52:21 -0400

    getting image upload to work

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./src/routes";
+import fileUpload from "express-fileupload";
 
 import bodyParser from "body-parser";
 const cors = require("cors");
@@ -8,10 +9,13 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(fileUpload());
 
 // Routes
 app.use(router);
 
 app.listen(process.env.PORT || 8080, () => {
-	console.log(`instaheat-server listening at http://localhost:${process.env.PORT || 8080}`);
+  console.log(
+    `instaheat-server listening at http://localhost:${process.env.PORT || 8080}`
+  );
 });
--- src/routes/images.ts
+++ src/routes/images.ts
@@ -2,26 +2,17 @@ import Router from "express-promise-router";
 import { getStorage, ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { v4 as uuidv4 } from "uuid";
+import { firebaseConfig } from "../config";
 
 const images = Router();
 
-const firebaseConfig = { 
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-};
-
 !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-images.post("/upload-images", async (req, res) => {
-  const blob = req.body;
+images.post("/upload-images", async (req: any, res) => {
+  const file = req?.files?.file;
   try {
     const fileRef = ref(getStorage(), uuidv4());
-    await uploadBytesResumable(fileRef, blob);
-
+    await uploadBytesResumable(fileRef, file.data);
     const downloadUrl = await getDownloadURL(fileRef);
     res.json(downloadUrl);
   } catch (e) {
