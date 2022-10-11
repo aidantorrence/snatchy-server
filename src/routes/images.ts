d281ca5a4c17f3364296bd0e3952e77d897429ab
Aidan Torrence <aidan.torrence@gmail.com>
2022-10-11 03:11:55 -0400

    fix

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -6,12 +6,12 @@ import { v4 as uuidv4 } from "uuid";
 const images = Router();
 
 const firebaseConfig = { 
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 !getApps().length ? initializeApp(firebaseConfig) : getApp();
