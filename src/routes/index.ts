4ee40be78a9c0b62fcb45fa6063a0fc9a0343d4e
Aidan Torrence <aidan.torrence@gmail.com>
2022-11-06 19:46:51 -0500

    delete

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,7 @@
 import { Router } from "express";
 import comments from "./comments";
 import images from "./images";
-import offers from "./offers";
 import outfits from "./outfits";
-import sendGrid from "./sendgrid";
-import s from "./stripe";
-import trades from "./trades";
 import users from "./users";
 
 const router = Router();
@@ -15,6 +11,6 @@ router.use(users);
 router.use(comments);
 // router.use(s);
 // router.use(sendGrid);
-// router.use(images);
+router.use(images);
 
 export default router;
